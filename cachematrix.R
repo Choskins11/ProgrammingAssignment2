787f5305697a8fb97dbeda2597014ee864685a80
Choskins <choskins11@hotmail.com>
2016-10-15 21:31:44 -0400

    Assignement 2 code edit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The fuctions take a square matix and returns the inverse
+## matrix stored in the parent environment. 
+## 
+## makeCacheMatrix initializes elements x and m and sets them
+## in memory in the parent environment. It then returns a list
+## of functions to the parent environment to be used downstream
 makeCacheMatrix <- function(x = matrix()) {
-
+              m <- NULL
+              set <- function(y) {
+                      x <<- y
+                      m <<- NULL
+               }
+  
+              get <- function() x
+              setinverse <- function(solve) m <<- solve
+              getinverse <- function() m
+              list(set = set, get = get,
+                    setinverse = setinverse,
+                    getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve works using solve()that returns the inverse of
+## a matrix. It checks to see if a result for m exists and if
+## so, returns it. Otherwise it sets the return of the solve()
+## This function only works with the list created by
+## makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+              m <- x$getinverse()
+              if(!is.null(m)) {
+                      message("getting cached data")
+                      return(m)
+              }
+              data <- x$get()
+              m <- solve(data, ...)
+              x$setinverse(m)
 }
